9edb79b6737877bf948b0d6fd72cfec188b2b42f
daithi <daithi@daithi-laptop>
2012-09-29 18:21:35 +0100

    gdrive class

---
+++ application/modules/ModalGdrive.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace CityIndex\WP\PostExternal\Modules;
+use CityIndex\WP\PostExternal\Controller;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ModalGdrive
+ *
+ * @author daithi
+ */
+class ModalGdrive extends Controller{
+	
+	public function __construct(){
+		
+		parent::__construct(__CLASS__);
+	}
+}
+
+?>
