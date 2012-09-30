c9f1361d573f660755617cf6801747a886a94265
daithi <daithi@daithi-laptop>
2012-09-30 18:32:18 +0100

    set up google app

--- application/modules/ModalGdrive.class.php
+++ application/modules/ModalGdrive.class.php
@@ -73,7 +73,7 @@ class ModalGdrive extends Controller{
 			'client_secret' => $this->client_secret,
 			'scope' => $this->scope,
 			'redirect_uri' => $this->redirect_uri,
-			'grant_type' => 'refresh_token'
+			'grant_type' => 'authorization_code'
 		);
 		
 		$ch = curl_init();
