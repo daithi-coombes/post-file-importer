25371a7f7735a8d697dce41eeb60fdacca49a26e
daithi <daithi@daithi-laptop>
2012-09-30 21:23:19 +0100

    set up google app

--- application/modules/ModalGdrive.class.php
+++ application/modules/ModalGdrive.class.php
@@ -193,7 +193,8 @@ class ModalGdrive extends Controller{
 		//build html and return
 		foreach($res->items as $file){
 			if(is_string($file->title)) ar_print($file->title);
-			print $file->title;
+			$title = $file->title;
+			print $title;
 		}
 			
 		return "{$res}</ul>\n";
