56e553ed283273d2685346fefe3e986a49f7009a
daithi <daithi@daithi-laptop>
2012-10-01 14:15:45 +0100

    set up google app

--- application/modules/ModalGdrive.class.php
+++ application/modules/ModalGdrive.class.php
@@ -148,7 +148,7 @@ class ModalGdrive extends Controller{
 		//if no code or refresh token, then this is first run. Login link will be displayed in view file
 		if(@!$_REQUEST['code'] && !$this->refresh_token)
 			return false;
-		
+		ar_print($this);
 		//vars
 		$ch = curl_init();
 		$params = array();
