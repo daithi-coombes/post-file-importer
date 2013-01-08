098f669877a15e01fae723faf9dd5a1d3a245586
daithi <daithi@daithi-laptop>
2013-01-08 13:59:46 +0000

    twitter get results

--- application/modules/Twitter.class.php
+++ application/modules/Twitter.class.php
@@ -15,15 +15,12 @@ class Twitter {
 		global $API_Connection_Manager;
 		$service = $API_Connection_Manager->get_service('twitter/index.php');
 		
-		$tweets = $service->request(
-			'http://api.twitter.com/1/statuses/user_timeline.format',
-			'GET',
-			array(
-				'user_id' => $service->user_id
-			)
+		$response = $service->request(
+			"https://api.twitter.com/1/statuses/friends_timeline.json",
+			'GET'
 		);
-		
-		ar_print($tweets);
+		$res = json_decode($response['body']);
+		ar_print($res);
 	}
 }
 
